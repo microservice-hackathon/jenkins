16a6dfd3cb65d44038d45ed3f789e2555bc5056f
Lukasz Szczesny <luk@wybcz.pl>
2015-03-24 20:09:42 +0100

    Typo

--- jobs/boot-microservice.groovy
+++ jobs/boot-microservice.groovy
@@ -1,15 +1,15 @@
 import org.apache.commons.lang.WordUtils
 
-def project = "4finance/boot-microservice"
-def branchApi = new URL('https://api.github.com/repos/${project}/branches')
+def project = '4finance/boot-microservice'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 
 branches.each {
     def branchName = it.name
-    def jobName = '${project}-${branchName}'.replaceAll("/","-");
+    def jobName = "${project}-${branchName}".replaceAll('/','-');
     job(jobName) {
         scm {
-            git('git://github.com/${project}.git', branchName)
+            git("git://github.com/${project}.git", branchName)
         }
         steps {
             gradle('clean build')
