4ca30361c99b01942469b9cd0bb1c3e90ebdeb71
Lukasz Szczesny <lukasz.szczesny@4finance.com>
2015-03-25 10:11:27 +0100

    Typo

--- jobs/boot-microservice.groovy
+++ jobs/boot-microservice.groovy
@@ -7,7 +7,7 @@ repos.each {
     def projectName = it.name
     def projectGitRepo = it.clone_url
 
-    if (projectName == "${organization}.github.io" || projectName = "properties") {
+    if (projectName == "${organization}.github.io" || projectName == "properties") {
         return
     }
     
