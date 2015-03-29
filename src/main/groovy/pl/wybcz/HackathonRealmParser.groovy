2281bd697b1bd7821d156bebd528856aaf8018e7
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2015-03-30 00:25:35 +0200

    Simplify multimap creation

--- src/main/groovy/pl/wybcz/HackathonRealmParser.groovy
+++ src/main/groovy/pl/wybcz/HackathonRealmParser.groovy
@@ -3,13 +3,10 @@ package pl.wybcz
 class HackathonRealmParser {
 
     Map<String, List<String>> convertToRealmMultimap(List projectToCode) {
-        return projectToCode.inject([:]) { acc, entry ->
+        Map emptyMultimap = [:].withDefault { [] }
+        return projectToCode.inject(emptyMultimap) { Map acc, entry ->
             String realm = entry.name.split('-').last()
-            if(acc[realm] == null) {
-                acc[realm] = [entry.name]
-            } else {
-                acc[realm] << entry.name
-            }
+            acc[realm] << entry.name
             return acc
         }
     }
