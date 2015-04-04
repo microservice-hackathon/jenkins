3d9cf5915cca02bb792a18ee42bb00afd1e8996e
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-04-04 21:18:55 +0200

    Added test stash project

---
+++ jobs/microservice-template-stash-test.groovy
@@ -0,0 +1,23 @@
+import pl.wybcz.pipeline.domain.GitProject
+import pl.wybcz.pipeline.template.MicroserviceTemplateBuilder
+
+def stashServerHost = '52.17.120.44:7990'
+def stashProjectCode = 'PRs'
+
+MicroserviceTemplateBuilder.pipeline(this) {
+    forProjects([new GitProject('test-repo', "http://${STASH_USERNAME}@$stashServerHost/scm/prs/test-repo.git")])
+    buildStashPrs {
+        stashHost stashServerHost
+        cronToPollScm '*/2 * * * *'
+        repoName 'test-repo'
+        projectCode stashProjectCode
+        username "${STASH_USERNAME}"
+        password "${STASH_PASSWORD}"
+    }
+    buildJobs()
+    buildViews { List<GitProject> project ->
+         return [stash : [project.name]]
+    }
+}
+
+
