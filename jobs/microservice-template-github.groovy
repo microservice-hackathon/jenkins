0aec875c95fab5d124f872f7c1c3f64961cf3205
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-06-16 21:35:25 +0200

    added projects to exclude

--- jobs/microservice-template-github.groovy
+++ jobs/microservice-template-github.groovy
@@ -5,8 +5,9 @@ import com.ofg.pipeline.template.MicroserviceTemplateBuilder
 def organization = binding.variables['ORGANIZATION_NAME'] ?: 'microhackathon-test'
 def reposApi = new URL("https://api.github.com/orgs/${organization}/repos")
 def repos = new GitProjectFetcher(binding.variables['TEST_MODE'] ?: false, reposApi).fetchRepos()
+def projectsToExclude =  ((binding.variables['PROJECTS_TO_EXCLUDE'] as String)?.split(',') as List) ?: ["${organization}.github.io", 'properties']
 
-List projectToCode = repos.findAll {!(it.name == "${organization}.github.io" || it.name == "properties")}
+List projectToCode = repos.findAll {!projectsToExclude.contains(it.name)}
 
 MicroserviceTemplateBuilder.pipeline(this) {
     forProjects projectToCode.collect { new GitProject(it.name, it.clone_url) }
