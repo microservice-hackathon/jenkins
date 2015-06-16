5cc07bdb42a8355c0be2bd2cf150384102ea3bab
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-06-16 21:24:49 +0200

    Added organization name as a var

--- jobs/microservice-template-github.groovy
+++ jobs/microservice-template-github.groovy
@@ -2,7 +2,7 @@ import com.ofg.pipeline.domain.GitProject
 import com.ofg.pipeline.domain.GitProjectFetcher
 import com.ofg.pipeline.template.MicroserviceTemplateBuilder
 
-def organization = 'microhackathon-test'
+def organization = binding.variables['ORGANIZATION_NAME'] ?: 'microhackathon-test'
 def reposApi = new URL("https://api.github.com/orgs/${organization}/repos")
 def repos = new GitProjectFetcher(binding.variables['TEST_MODE'] ?: false, reposApi).fetchRepos()
 
