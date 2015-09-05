2542f5ffcd156559658d2cb4496ab39787fa0b7b
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-09-05 22:37:29 +0200

    Fixed too many repos generated

--- jobs/microservice-template-github.groovy
+++ jobs/microservice-template-github.groovy
@@ -7,7 +7,7 @@ def reposApi = new URL("https://api.github.com/orgs/${organization}/repos")
 def repos = new GitProjectFetcher(binding.variables['TEST_MODE'] ?: false, reposApi).fetchRepos()
 def projectsToExclude =  ((binding.variables['PROJECTS_TO_EXCLUDE'] as String)?.split(',') as List) ?: ["${organization}.github.io", 'properties']
 
-List projectToCode = repos.findAll {!projectsToExclude.contains(it.name)}
+List projectToCode = repos.findAll {def project -> !projectsToExclude.any { project.name.contains(it) } }
 
 MicroserviceTemplateBuilder.pipeline(this) {
     forProjects projectToCode.collect { new GitProject(it.name, it.clone_url) }
